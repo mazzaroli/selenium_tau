c0dff4940940a7b791e88ac00089e28c7402788c
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-19 15:00:49 -0300

    Refactor HomePage class for improved link clicking abstraction
    Enhanced the HomePage class to improve code maintainability and readability by centralizing link clicking functionality. Instead of individual methods for each link, introduced a private method clickLink(String linkText) within the HomePage class. This method efficiently clicks links based on the provided link text.
    
    Changes:
    - Modified the HomePage class to utilize a unified clickLink method for clicking different links.
    - Replaced individual click methods (e.g., clickFormAuthentication, clickDropdown, clickForgotPassword) with a generalized clickLink method that accepts link text as a parameter.
    - Encapsulated the repetitive link clicking logic within the clickLink method, simplifying code redundancy and enhancing maintainability.
    - Ensured the HomePage class remains more adaptable to potential changes in the web application's links, reducing the impact of future updates.
    
    Note: The refactored HomePage class aims to streamline link clicking operations, promoting cleaner and more concise code architecture.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,15 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
-    private WebDriver driver;
-    private By formAuthenticationLink = By.linkText("Form Authentication");
+    private final WebDriver driver;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
     public LoginPage clickFormAuthentication(){
-        driver.findElement(formAuthenticationLink).click();
+        clickLink("Form Authentication");
         return new LoginPage(driver);
     };
+
+    public DropdownPage clickDropdown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    };
+
+    public ForgotPasswordPage clickForgotPassword(){
+        clickLink("Forgot Password");
+        return new ForgotPasswordPage(driver);
+    };
+
+    private void clickLink(String linkTest){
+        driver.findElement(By.linkText(linkTest)).click();
+    }
 }
