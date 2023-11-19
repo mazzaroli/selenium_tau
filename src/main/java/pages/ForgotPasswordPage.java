9cd9ab5dc455a73d9c6815c593473f85eec11a9e
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-19 14:59:13 -0300

    Implement ForgotPasswordPage class for password retrieval flow
    Added ForgotPasswordPage class to handle the password retrieval process. This class encapsulates functionalities to input the email and retrieve the password through the provided email address.
    
    Changes:
    - Introduced ForgotPasswordPage class to manage the password retrieval flow.
    - Implemented enterEmail() to input the email address into the respective field.
    - Included clickRetrievePassword() to trigger the password retrieval action by clicking the 'Retrieve Password' button.
    - Provided retrievePassword() method as a combined action for entering the email and initiating password retrieval.
    
    Note: The ForgotPasswordPage class simplifies the password retrieval process by offering methods for individual steps and a combined action for ease of use.

---
+++ src/main/java/pages/ForgotPasswordPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ForgotPasswordPage{
+    private final WebDriver driver;
+    private final By emailField = By.id("email");
+    private final By retrievePasswordButton = By.id("form_submit");
+
+    public ForgotPasswordPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterEmail(String email){
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public EmailSentPage clickRetrievePassword(){
+        driver.findElement(retrievePasswordButton).click();
+        return new EmailSentPage(driver);
+    }
+
+    public EmailSentPage retrievePassword(String email){
+        enterEmail(email);
+        return clickRetrievePassword();
+    }
+}
