a599fc4ccc4b64bda47cf8aea58ae425ec4f9346
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-19 15:01:58 -0300

    Refactor: Updated HomePage class to utilize a clickLink method for common link clicks
    The HomePage class has been refactored to centralize the click action for common links. Instead of repetitive code to click different links, a new private method clickLink was introduced to streamline the process. Now, the clickFormAuthentication, clickDropdown, and clickForgotPassword methods utilize this common function to interact with respective links by their link text.
    
    Old behavior: Each method in HomePage class used separate code for link clicks.
    New behavior: Implemented a central clickLink method to click various links in HomePage class.
    
    Changes Made:
    - Consolidated repetitive code by introducing clickLink method.
    - Refactored clickFormAuthentication, clickDropdown, and clickForgotPassword methods to use the clickLink function.
    
    This refactor enhances maintainability and readability of the HomePage class.

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-    private WebDriver driver;
-    private By usernameField = By.id("username");
-    private By passwordField = By.id("password");
-    private By loginButton = By.cssSelector("#login button");
+    private final WebDriver driver;
+    private final By usernameField = By.id("username");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.cssSelector("#login button");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
