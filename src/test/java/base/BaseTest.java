636cc863c938a678f71ded500e8c1e5d5cf823b8
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-19 14:55:24 -0300

    Refactor test setup using TestNG annotations
    Updated the test setup method in the BaseTest class to utilize TestNG annotations (@BeforeClass) for initializing the WebDriver instance and navigating to the specified URL. This enhancement ensures a more organized and streamlined test configuration process, promoting better code readability and maintainability.
    
    Changes:
    - Replaced the setUp() method with a @BeforeClass annotated method to set up WebDriver and navigate to the test URL.
    - Initialized the WebDriver instance within the @BeforeClass method using ChromeDriver.
    - Created a reference to the HomePage class for page object initialization.
    
    Note: The tearDown() method is commented out for now to prevent premature browser closure. Uncomment it for proper teardown after test execution.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,27 +1,26 @@
 package base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 public class BaseTest {
     private WebDriver driver;
     protected HomePage homePage;
 
-    public void setUp(){
+    @BeforeClass
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
         homePage = new HomePage(driver);
-
-        driver.quit();
     }
 
-    public static void main(String args[]){
-        BaseTest test = new BaseTest();
-        test.setUp();
-    }
+//    @AfterClass
+//    public void tearDown(){
+//        driver.quit();
+//    }
 }
